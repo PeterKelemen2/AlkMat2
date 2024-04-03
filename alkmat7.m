bc11b77d0c5203c03b2b98d8d0ae9ec29f778080
PeterKelemen2 <90985214+PeterKelemen2@users.noreply.github.com>
2024-04-03 15:39:20 +0200

    Add files via upload

--- alkmat7.m
+++ alkmat7.m
@@ -5,7 +5,7 @@ s = [15, 4  1  7  4 12  9, 8];
 
 xx = linspace(-2.1, 3.1);
 yy = spline(x,s,xx);
-plot(x,s(2:end-1), '*', xx, yy);
+#plot(x,s(2:end-1), '*', xx, yy);
 
 # p = spline(x,s)
 
@@ -14,19 +14,19 @@ clear;
 
 F = @(x) 1 ./ (1 + 25 * x.^2);
 x = linspace(-1,1);
-plot(x, F(x), 'k')
-hold on;
+#plot(x, F(x), 'k')
+#hold on;
 
 xx = -1:0.2:1;
 p = polyfit(xx, F(xx), length(xx)-1);
-plot(x, polyval(p,x), 'r');
+#plot(x, polyval(p,x), 'r');
 
 xx = -1:0.2:1;
 y = [0, F(xx), 0];
-plot(x, spline(xx,y,x), 'g');
-legend('f', 'Lagrange', 'Spline');
+#plot(x, spline(xx,y,x), 'g');
+#legend('f', 'Lagrange', 'Spline');
 
-hold off;
+#hold off;
 
 # 5. Feladat (szorgalmi) =======================================================
 clear;
@@ -41,4 +41,42 @@ y = [0, 0, 1, 2.5, 0]; # A Két szélén az egyenesek deriváltja (meredeksége)
 xx = linspace(0,2);
 yy = spline(x, y, xx);
 
-plot(xx, yy);
+#plot(xx, yy);
+
+# ==============================================================================
+# +---------------+
+# | Leslie-modell |
+# +---------------+
+
+# Oszlopok a korábbi időszaknak az egyedei
+# Sorok száma  az egyedek korát jelenti
+# 1év   2év  3év   - ből
+#|   0     6   8 | # Mennyi 1
+#| 0.5     0   0 | #        2
+#|   0   0.5   0 | #        3 éves lesz
+
+A = [0 6 0; 0.5 0 0; 0 0.5 0];
+# A * nyulak     # Egy év múlva és
+# A * A * nyulak # Két év múlva mennyi lesz az egyedszám
+
+u = eig(A) # Saját értékek vektora
+[U,V] = eig(A) # Így két mátrixot ad vissza
+U(:,1) # 1. saját értékhez tartozó vektor
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

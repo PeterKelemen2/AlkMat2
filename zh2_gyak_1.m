12f9b553f6a7f2bf80f8ed6f0d9df19377e45b0e
PeterKelemen2 <90985214+PeterKelemen2@users.noreply.github.com>
2024-05-15 12:26:55 +0200

    Add files via upload

---
+++ zh2_gyak_1.m
@@ -0,0 +1,86 @@
+clc;
+clear;
+# Sajátérték gyakorlás
+
+# 1. Feladat
+#A = [0,   0,   0,   1/2;
+#	 1/2, 0,   1/2, 0;
+#	 1/2, 1/2, 0,   1/2;
+#	 0,   1/2, 1/2, 0];
+#[V, U] = eig(A);
+#x = V(:,1);
+#
+#A*x;
+#y = [0.2217 0.0308 0.4102 0.3373]';
+#y = A*y
+#y = A*y
+#y = A*y
+#y = A*y
+#y = A*y
+
+# 2. Feladat
+#A = [0 4 5;
+#	 0.8 0 0;
+#	 0 0.4 0];
+
+# 3. Feladat
+#A = [-1 1 0; 3 -3 1; 1 3 0];
+#v = [4 0 1]';
+
+# 4. Feladat
+A = [0.5  0.4  0.1;
+	 0.3  0.4  0.2;
+	 0.2  0.2  0.7];
+[U,V] = eig(A);
+x = abs(U(:,1));
+
+# 5. Feladat
+# Egészítse ki az alábbi x vektort úgy, hogy az A mátrix abszolútértékben
+# legkisebb sajátértékéhez tartozó sajátvektor legyen!
+A = [73 32 19 10;
+	 -73 -32 -17 -11;
+	 -116 -50 -32 -16;
+	 -88 -40 -26 -9];
+# Megoldás: x = [-1 2 0 1]'
+
+[V,U] = eig(A);
+V
+v = V(:,2)
+l = 1/v(4) # 3. nem 0
+x = v*l # x = [-1 2 -6.8783e-13 1]'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
